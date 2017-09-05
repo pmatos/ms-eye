b5dd5a0f8c21ef4e60d56de74be03006202bf2e1
Paulo Matos <pmatos@linki.tools>
2017-09-05 20:53:35 +0200

    Move mseye file to __init__ and add eye of thundera image.

--- mseye/__init__.py
+++ mseye/__init__.py
@@ -1,7 +1,42 @@
-# -*- coding: utf-8 -*-
-import pkg_resources
+from flask import Flask
+from flask_script import Manager
+import mongoengine as me
+import requests
+from PIL import Image
+from io import BytesIO
+from eyesnap import EyeSnap
+from datetime import datetime
+import hashlib
+import binascii
+import os
 
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except:
-    __version__ = 'unknown'
+mseye = Flask(__name__)
+manager = Manager(mseye)
+
+DATA_DIR=os.path.join(mseye.root_path, 'data')
+
+me.connect('mseye')
+
+
+@mseye.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@mseye.route('/sight-beyond-sight')
+def get_current_image():
+    r = requests.get('http://192.168.178.56/jpg/image.jpg', auth=('pmatos', 'iamIr0nman'))
+    print(r.status_code)
+    print(r.encoding)
+
+    stream = BytesIO(r.content)
+    image = Image.open(stream)
+    checksum = hashlib.sha256(stream.getvalue()).digest()
+    filename = '.'.join([binascii.hexlify(checksum).decode('utf-8'), 'jpg'])
+    path = os.path.join(DATA_DIR, filename)
+    image.save(path)
+
+    es = EyeSnap(datetime.now(), path)
+    es.save()
+
+    return render_template('sight.html', eyeimg=url_for('static', 'eye.jpg'), snap=es)
--- mseye/eyesnap.py
+++ mseye/eyesnap.py
@@ -1,5 +1,6 @@
 import mongoengine as me
 
+
 class EyeSnap(me.Document):
     timestamp = me.DateTimeField(required=True)
     path = me.StringField(required=True)
